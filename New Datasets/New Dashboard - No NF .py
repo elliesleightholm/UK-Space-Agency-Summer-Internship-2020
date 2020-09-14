5ba6acae6c53cfe489d76552c0533d6fd27d7455
Ellie Sleightholm <59167495+elliesleightholm@users.noreply.github.com>
2020-09-14 10:43:36 +0100

    Update and rename ND New Dashboard - No NF .py to New Dashboard - No NF .py

--- New Datasets/New Dashboard - No NF .py
+++ New Datasets/New Dashboard - No NF .py
@@ -3,15 +3,6 @@ import dask.dataframe as dd # this module will allow us to turn our dataset into
 import numpy as np # importing a module that will allow us to assign a specific colour range
 import matplotlib.pyplot as plt # will allow us to plot
 
-# New datasets:
-    # 171103T000001                                     - Range Resolution: 75m, Maximum Range: 1200m, velocity = 0.078125 .
-    # 171110T000001                                     - Range Resolution: 75m, Maximum Range: 1200m, velocity = 0.109375
-    # 171127T000524 - has some but not massses of data  - Range Resolution: 75m, Maximum Range: 1200m, velocity = 0.078125 * 2 .
-    # 171128T000002 - has some but not masses of data   - Range Resolution: 75m, Maximum Range: 1200m, velocity = 0.078125 * 2 .
-    # 171206T001724                                     - Range Resolution: 60m, Maximum Range: 960m, velocity = 0.078125 * 2 .
-    # 180129T000002 - has little data                   - Range Resolution: 60m, Maximum Range: 960m, velocity = 0.078125 * 2 .
-    # 180227T000003                                     - Range Resolution: 60m, Maximum Range: 960m, velocity = 0.078125 .
-
 # Give specific n value and time step for scatter plot
 n = 1550
 t = 1
